e0c2ad9256949717d786db2aa1f83f0a8354028c
Sameermd25 <149755097+Sameermd25@users.noreply.github.com>
2025-09-02 16:11:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,27 +2,27 @@
 const d1=document.getElementById("square1");
 const d2=document.getElementById("square2");
 const d3=document.getElementById("square3");
-d1.addEventListener("mouseenter",function() {
+d1.addEventListener("mouseover",function() {
 	d2.style.backgroundColor="#6F4E37";
 	d3.style.backgroundColor="#6F4E37";
 })
-d1.addEventListener("mouseleave",function() {
+d1.addEventListener("mouseout",function() {
 	d2.style.backgroundColor="#E6E6FA";
 	d3.style.backgroundColor="#E6E6FA";
 })
-d2.addEventListener("mouseenter",function() {
+d2.addEventListener("mouseover",function() {
 	d1.style.backgroundColor="#6F4E37";
 	d3.style.backgroundColor="#6F4E37";
 })
-d2.addEventListener("mouseleave",function() {
+d2.addEventListener("mouseout",function() {
 	d1.style.backgroundColor="#E6E6FA";
 	d3.style.backgroundColor="#E6E6FA";
 })
-d3.addEventListener("mouseenter",function() {
+d3.addEventListener("mouseover",function() {
 	d2.style.backgroundColor="#6F4E37";
 	d1.style.backgroundColor="#6F4E37";
 })
-d3.addEventListener("mouseleave",function() {
+d3.addEventListener("mouseout",function() {
 	d2.style.backgroundColor="#E6E6FA";
 	d1.style.backgroundColor="#E6E6FA";
 })
